8b9dc55f4c82de867e0deb5da416214975aec139
Syeda Maham Fahim <syedamahamfahim@gmail.com>
2021-11-20 21:52:25 +0500

    first-commit

---
+++ src/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = ({val}) => {
+    return (
+        <>
+            <h1>{val.id}</h1>
+            <h1>{val.title}</h1>
+        </>
+    )
+}
+
+export default Basic
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        id:1,
+        title:"Web",
+        img:''
+    },
+    {
+        id:2,
+        title:"Mob",
+        img:''
+    },
+    {
+        id:3,
+        title:"Docker",
+        img:''
+    },
+    {
+        id:4,
+        title:"Github",
+        img:''
+    },
+]
+export default data;
