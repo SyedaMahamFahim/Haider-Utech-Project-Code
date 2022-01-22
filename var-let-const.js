6938cf7ccbe7685e6f20139e7368ad30f792e3a1
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-01-22 15:41:05 +0500

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,17 @@
+// var x=9
+
+// var x=10
+// console.log("this is the value of x using var",x)
+
+// let x=9
+// x=10
+// console.log("this is the value of x using let",x)
+
+
+const x=9
+console.log("this is the value of x using const ",x)
+
+
+// var modify -redefine 
+// let modify but can be redefine
+// const cant be modify neither it can redefine
