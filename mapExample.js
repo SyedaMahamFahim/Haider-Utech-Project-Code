4a6e644ebe82b27f68b861f05b04ad5e25e6bac6
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-05-29 17:19:58 +0500

    map example

---
+++ mapExample.js
@@ -0,0 +1,17 @@
+const cars = [
+  "suzuki", // string
+  9,       // number
+  "corrola",  // string
+  true, // boolean
+  undefined, // undefined
+  {
+    id: 1,
+    name: "hey",
+  }, //object
+];
+
+console.log("cars", cars);
+
+// console.log(cars[0])
+console.log(cars[1])
+
